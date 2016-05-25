b523ffe58e8e5d38c96a58de4243f7bad052c472
Daniel Frederick Crisman <daniel.crisman@gmail.com>
2016-05-24 21:07:48 -0400

    location in cards and comments for how to compute the calls

--- rdoorlib.py
+++ rdoorlib.py
@@ -7,9 +7,12 @@ class GHCard:
     # This is the attribute byte, but only item defined is bit 0 == 1 is access allowed, 0 is denied
     access = None
     # facility code (FC)
-    facility = None
+    fc = None
     # card code (CC)
-    card = None
+    cc = None
+
+    # location in memory on the nano
+    loc = None
 
     # if new ACL list says we are denied
     set_denied = False
@@ -20,19 +23,26 @@ class GHCard:
     # if processed in new ACL list
     done = False
 
-    def __init__( self, access, facilityCode, cardCode ):
+    def __init__( self, access, facilityCode, cardCode, location=None ):
         self.access = access
         self.fc = facilityCode
         self.cc = cardCode
+        self.loc = location
 
     def __eq__( self, other ):
         if type(self) is type(other):
-            return self.facility == other.facility and self.card == other.card
+            return self.fc == other.fc and self.cc == other.cc
         else:
             return False
 
 class GHACL:
     acl = None
+    # start location fo the ACL list on nan0
+    start = None
+    # end location of the ACL list on nano
+    #   XXX verify this, but I think this is the first address not used by 
+    #   the ACL list (e.i. pointer just after the last record)
+    end = None
 
     def __init__( self, acl=None ):
         if acl is None:
@@ -71,3 +81,11 @@ class GHACL:
             if o.access:
                 # no card matched in new ACL list, disable it
                 o.set_denied = True
+        # how to figure out what the spare record spaces at the end are?
+        # find the new record spots at the end for the add list to get locations from
+        ## loop on self.acl to find items with set_denied/allowed to create calls for
+        ## loop on add list to create calls for adding them
+        ## add set acl_end if we need more space
+            ## add spaceholder values for the last bit of the ACL list if we add extra allocations at the end
+
+        ## loop and run all the calls to the nano
